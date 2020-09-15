85e50b0a20510764128a692db752e2c14f93e987
Bart Schuurmans <bart@hexon.nl>
2020-09-15 10:34:35 +0200

    Hard tab does not occur in newer versions of Go test

--- parser/parser.go
+++ parser/parser.go
@@ -66,7 +66,7 @@ var (
 	regexResult   = regexp.MustCompile(`^(ok|FAIL|\?)\s+([^ ]+)\s+(?:(\d+\.\d+)s|\(cached\)|\[no test files\]|(\[\w+ failed]))(?:\s+coverage:\s+(\d+\.\d+)%\sof\sstatements(?:\sin\s.+)?)?$`)
 	// regexBenchmark captures 3-5 groups: benchmark name, number of times ran, ns/op (with or without decimal), B/op (optional), and allocs/op (optional).
 	regexBenchmark       = regexp.MustCompile(`^(Benchmark[^ -]+)(?:-\d+\s+|\s+)(\d+)\s+(\d+|\d+\.\d+)\sns/op(?:\s+(\d+)\sB/op)?(?:\s+(\d+)\sallocs/op)?`)
-	regexOutput          = regexp.MustCompile(`(    )*\t(.*)`)
+	regexOutput          = regexp.MustCompile(`(    )*\t?(.*)`)
 	regexSummary         = regexp.MustCompile(`^(PASS|FAIL|SKIP)$`)
 	regexPackageWithTest = regexp.MustCompile(`^# ([^\[\]]+) \[[^\]]+\]$`)
 )
